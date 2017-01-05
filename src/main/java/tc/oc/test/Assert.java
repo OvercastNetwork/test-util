d6cad58c5a84ea548baac3eb178ddedc22c05300
Jedediah Smith <jedediah@silencegreys.com>
2017-01-05 15:52:00 -0500

    Add some stuff from private repos

--- src/main/java/tc/oc/test/Assert.java
+++ src/main/java/tc/oc/test/Assert.java
@@ -3,6 +3,15 @@ package tc.oc.test;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Range;
+import com.google.common.reflect.TypeToken;
+import org.apache.commons.lang.StringEscapeUtils;
 
 import static org.junit.Assert.*;
 
@@ -31,12 +40,61 @@ public final class Assert {
         assertEquals(Arrays.asList(expected), actual);
     }
 
-    public static void assertThrows(Class<? extends Throwable> expected, TestCodeBlock block) throws Throwable {
+    public static void assertEqualSequences(Iterable<?> expecteds, Iterable<?> actuals) {
+        assertEquals(ImmutableList.copyOf(expecteds), ImmutableList.copyOf(actuals));
+    }
+
+    public static void assertSequence(Iterable<?> actuals, Object... expecteds) {
+        assertEqualSequences(ImmutableList.copyOf(expecteds), actuals);
+    }
+
+    public static void assertSequence(Stream<?> actuals, Object... expecteds) {
+        assertEqualSequences(ImmutableList.copyOf(expecteds), actuals.collect(Collectors.toList()));
+    }
+
+    public static void assertSequence(Iterable<?> expecteds, Stream<?> actuals) {
+        assertEqualSequences(ImmutableList.copyOf(expecteds), actuals.collect(Collectors.toList()));
+    }
+
+    public static void assertEqualSets(Iterable<?> expecteds, Iterable<?> actuals) {
+        assertEquals(ImmutableSet.copyOf(expecteds), ImmutableSet.copyOf(actuals));
+    }
+
+    public static void assertSet(Iterable<?> actuals, Object... expecteds) {
+        assertEqualSets(ImmutableSet.copyOf(expecteds), actuals);
+    }
+
+    public static <T extends Comparable<T>> void assertContains(Range<T> range, T value) {
+        if(!range.contains(value)) {
+            throw new AssertionError("Expected a value in the range " + range + " but actual value is " + value);
+        }
+    }
+
+    public static void assertSubstring(String part, String whole) {
+        if(!whole.contains(part)) {
+            fail("Expected \"" + StringEscapeUtils.escapeJava(part) +
+                 "\" to be contained in \"" + StringEscapeUtils.escapeJava(whole) + "\"");
+        }
+    }
+
+    public static void assertInstanceOf(Class<?> type, Object obj) {
+        if(!type.isInstance(obj)) {
+            fail("Expected instance of " + type.getName() + " instead of " + (obj == null ? "null" : "a " + obj.getClass().getName() + ": " + obj));
+        }
+    }
+
+    public static void assertAssignableTo(TypeToken<?> to, TypeToken<?> type) {
+        if(!to.isAssignableFrom(type)) {
+            fail("Expected " + type + " to be assignable to " + to);
+        }
+    }
+
+    public static void assertThrows(Class<? extends Throwable> expected, TestCodeBlock block) {
         try {
             block.run();
         } catch(Throwable throwable) {
             if(expected.isInstance(throwable)) return;
-            throw throwable;
+            throw Throwables.propagate(throwable);
         }
         fail("Expected " + expected.getSimpleName() + " to be thrown, but nothing was thrown");
     }
